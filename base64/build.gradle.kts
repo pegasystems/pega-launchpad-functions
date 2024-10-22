39330d6424d4e0527bf35f1ea89246c16f28bf51
Tim Miranda <tim.miranda@gmail.com>
2024-10-22 16:56:33 -0400

    increment library versions

--- base64/build.gradle.kts
+++ base64/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.pega.launchpad.base64"
-version = "0.1.7-SNAPSHOT"
+version = "0.1.8-SNAPSHOT"
 
 repositories {
     mavenCentral()
--- text/build.gradle.kts
+++ text/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.pega.launchpad.text"
-version = "0.1.7-SNAPSHOT"
+version = "0.1.8-SNAPSHOT"
 
 repositories {
     mavenCentral()
