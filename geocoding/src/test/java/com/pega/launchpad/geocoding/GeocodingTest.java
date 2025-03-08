c873666c61d2ddf8c3cdcd36dccb642c6b75f989
Tim Miranda <tim.miranda@gmail.com>
2025-03-07 19:52:56 -0500

    add geocoding module, update dependencies

--- geocoding/src/test/java/com/pega/launchpad/geocoding/GeocodingTest.java
+++ geocoding/src/test/java/com/pega/launchpad/geocoding/GeocodingTest.java
@@ -2,16 +2,21 @@ package com.pega.launchpad.geocoding;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class GeocodingTest {
 
     @Test
     void getAddressComponents() {
 
-//        Map<String,String> inputMap = new HashMap<>();
-//        inputMap.put("Key", "");
-//        inputMap.put("Address", "21 Beech ST, North Chelmsford, MA 01863");
-//        System.out.println(Geocoding.getAddressComponents(inputMap));
-//        inputMap.put("Address", "Raheja Mindspace, SEZ, Building 12A, 11-14 th floors, Survey No - 64, Hitech City Rd, Madhapur, Telangana 500081, India");
-//        System.out.println(Geocoding.getAddressComponents(inputMap));
+        Map<String,String> inputMap = new HashMap<>();
+        inputMap.put("key", "1234");
+        inputMap.put("address", "21 Beech ST, North Chelmsford, MA 01863");
+        try {
+            System.out.println(Geocoding.getAddressComponents(inputMap));
+        } catch (RuntimeException ex) {
+            // ok
+        }
     }
 }
