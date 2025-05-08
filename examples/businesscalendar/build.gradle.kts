9f0c55470fbe4ab8ce72452239c3a5beace1dc51
GrzegH <grzegorz.herok@gmail.com>
2025-05-08 15:27:27 +0200

    Create build.gradle.kts

---
+++ examples/businesscalendar/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    id("base")
+}
+
+version = extra["PegaLaunchpadFunctionsGroupVersion"].toString() + "-SNAPSHOT";
+
+tasks.register<Zip>("businesscalendar") {
+    from("./src")
+    archiveFileName.set(project.name + "-" + project.version + ".zip")
+    destinationDirectory.set(layout.buildDirectory.dir("distributions"))
+}
+
+tasks.named("build") {
+    dependsOn("businesscalendar")
+}
