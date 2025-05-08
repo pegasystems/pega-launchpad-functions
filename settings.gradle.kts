d087d413b681addcb60358d79c05be8f37504195
GrzegH <grzegorz.herok@gmail.com>
2025-05-08 15:24:53 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,2 @@
 rootProject.name = "pega-launchpad-functions"
-include("examples:base64", "examples:email", "examples:parser", "examples:pdf", "examples:text", "examples:net", "examples:geocoding", "sdk:java", "examples:nodejs", "examples:python")
+include("examples:base64", "examples:email", "examples:parser", "examples:pdf", "examples:text", "examples:net", "examples:geocoding", "sdk:java", "examples:nodejs", "examples:python", "examples:businesscalendar")
